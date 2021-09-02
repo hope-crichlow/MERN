f6c85b1e6065bef4b898acc3738665d3124add44
hope-crichlow <hopecrichlow@gmail.com>
2021-09-01 18:31:53 -0700

    Result #2

--- javascript/fundamentals/practice/hoisting.js
+++ javascript/fundamentals/practice/hoisting.js
@@ -1,7 +1,7 @@
 // 1
 // GIVEN
-console.log(hello);
-var hello = "world";
+// console.log(hello);
+// var hello = "world";
 
 // AFTER HOISTING BY THE INTERPRETER
     // PREDICTION
@@ -15,15 +15,29 @@ var hello = "world";
 // 2
 
 // GIVEN
-// var needle = "haystack";
-// test();
-// function test() {
-// 	var needle = "magnet";
-// 	console.log(needle);
-// }
+var needle = "haystack";
+test();
+function test() {
+	var needle = "magnet";
+	console.log(needle);
+}
 // AFTER HOISTING BY THE INTERPRETER
 	// PREDICTION
-// var needle; // 'needle' is a declared, it's global and gets hoisted
-// needle = "haystack"; // assign the value of "haystack" to the global 'needle'
-// test(); // test is called and will throw a typeError saying that test isnt a function
+// OUTPUT: error
+
+// var needle;              // 'needle' is a declared, it's global and gets hoisted
+// needle = "haystack";     // assign the value of 'haystack' to the global 'needle'
+// test();                  // 'test()' is called and will throw a typeError saying that test isnt a function
+
 	// RESULT
+// OUTPUT: magnet
+
+// var needle;              // 'needle' is a declared, it's global and gets hoisted
+// needle = "haystack";     // assign the value of 'haystack' to the global 'needle'
+// function test() {        // 'test()' gets hoisted to the top
+// var needle;              // 'needle' is declared within 'test()' and is hoisted to the top of its scope
+// 	needle = "magnet";      // assign the value of 'magnet' to the function scoped 'needle'
+// 	console.log(needle);    // log 'needle' as 'magnet'
+// }
+// test();                  // 'test()' is called, the console.log runs and outputs 'magnet'
+
