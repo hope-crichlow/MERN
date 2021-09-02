c52287d1150d44b89c1c57b5224673b83f001a30
hope-crichlow <hopecrichlow@gmail.com>
2021-09-01 18:07:54 -0700

    Prediction #2

--- javascript/fundamentals/practice/hoisting.js
+++ javascript/fundamentals/practice/hoisting.js
@@ -10,3 +10,20 @@ var hello = "world";
 // var hello = "world";
     // RESULT
 // console.log(hello); // logs undefined
+
+
+// 2
+
+// GIVEN
+// var needle = "haystack";
+// test();
+// function test() {
+// 	var needle = "magnet";
+// 	console.log(needle);
+// }
+// AFTER HOISTING BY THE INTERPRETER
+	// PREDICTION
+// var needle; // 'needle' is a declared, it's global and gets hoisted
+// needle = "haystack"; // assign the value of "haystack" to the global 'needle'
+// test(); // test is called and will throw a typeError saying that test isnt a function
+	// RESULT
