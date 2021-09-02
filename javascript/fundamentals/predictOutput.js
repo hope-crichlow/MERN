fbb7f06a611d9da8886387ae4750a2e8a8396ba5
hope-crichlow <hopecrichlow@gmail.com>
2021-09-02 02:53:26 -0700

    Prediction #4

--- javascript/fundamentals/predictOutput.js
+++ javascript/fundamentals/predictOutput.js
@@ -3,8 +3,8 @@ const cars = ["Tesla", "Mercedes", "Honda"];
 const [randomCar] = cars;
 const [, otherRandomCar] = cars;
 //Predict the output
-console.log(randomCar);         // Tesla
-console.log(otherRandomCar);    // Mercedes
+console.log(randomCar);             // Tesla
+console.log(otherRandomCar);        // Mercedes
 
 // PROBLEM 2
 const employee = {
@@ -28,5 +28,14 @@ const person = {
 const password = "12345";
 const { password: hashedPassword } = person;
 //Predict the output
-console.log(password);          // 12345
-console.log(hashedPassword);    // undefined
+console.log(password);              // 12345
+console.log(hashedPassword);        // undefined
+
+//  PROBLEM 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [, first] = numbers;
+const [, , , second] = numbers;
+const [, , , , , , , , third] = numbers;
+//Predict the output
+console.log(first == second);       // false
+console.log(first == third);        // true
