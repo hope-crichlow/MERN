7fe5a29b455a152d079985c4e0d459c8a0a77bf2
hope-crichlow <hopecrichlow@gmail.com>
2021-09-02 02:39:49 -0700

    Prediction #1

--- javascript/fundamentals/predictOutput.js
+++ javascript/fundamentals/predictOutput.js
@@ -3,5 +3,5 @@ const cars = ["Tesla", "Mercedes", "Honda"];
 const [randomCar] = cars;
 const [, otherRandomCar] = cars;
 //Predict the output
-console.log(randomCar);
-console.log(otherRandomCar);
+console.log(randomCar);         // Tesla
+console.log(otherRandomCar);    // Mercedes
