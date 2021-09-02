91bb74115d6180fc7f5080ee3c2a885d8db537a6
hope-crichlow <hopecrichlow@gmail.com>
2021-09-01 17:51:11 -0700

    Prediction #1

--- javascript/fundamentals/practice/hoisting.js
+++ javascript/fundamentals/practice/hoisting.js
@@ -1,10 +1,13 @@
 // 1
-
 // GIVEN
+
 // console.log(example);
 // let example = "I'm the example!";
 
 // AFTER HOISTING BY THE INTERPRETER
-// PREDICTION
 
-// RESULT
+    // PREDICTION
+// console.log(example); // Error. Can't access 'example' before initialization
+// let example = "I'm the example!";
+
+    // RESULT
