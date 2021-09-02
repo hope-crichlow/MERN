ae0bda78368d7344a1741faed99a7894c7604782
hope-crichlow <hopecrichlow@gmail.com>
2021-09-01 19:22:05 -0700

    Indention

--- javascript/fundamentals/practice/hoisting.js
+++ javascript/fundamentals/practice/hoisting.js
@@ -35,14 +35,14 @@ function test() {
 	// RESULT
 // OUTPUT: magnet
 
-// var needle;              // 'needle' is a declared, it's global and gets hoisted
-// needle = "haystack";     // assign the value of 'haystack' to the global 'needle'
-// function test() {        // 'test()' gets hoisted to the top
-// var needle;              // 'needle' is declared within 'test()' and is hoisted to the top of its scope
+// var needle;                  // 'needle' is a declared, it's global and gets hoisted
+// needle = "haystack";         // assign the value of 'haystack' to the global 'needle'
+// function test() {            // 'test()' gets hoisted to the top
+// var needle;                  // 'needle' is declared within 'test()' and is hoisted to the top of its scope
 // 	needle = "magnet";      // assign the value of 'magnet' to the function scoped 'needle'
 // 	console.log(needle);    // log 'needle' as 'magnet'
 // }
-// test();                  // 'test()' is called, the console.log runs and outputs 'magnet'
+// test();                      // 'test()' is called, the console.log runs and outputs 'magnet'
 
 
 // 3
