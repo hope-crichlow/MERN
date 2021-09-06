56712c62475a928c3aa65ab119887693b66b61a6
hope-crichlow <hopecrichlow@gmail.com>
2021-09-05 18:21:03 -0700

    Created ninja class with required attributes

--- javascript/fundamentals/practice/ninja.js
+++ javascript/fundamentals/practice/ninja.js
@@ -1,3 +1,15 @@
+// Create a Ninja class
+class Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 10;
+        this.speed = 3;
+        this.strength = 3;
+    }
+}
+
+
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
