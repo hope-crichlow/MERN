b4c6a8b474a3f65631c150f6e3b51d0837c6e655
hope-crichlow <hopecrichlow@gmail.com>
2021-09-05 18:35:36 -0700

    showStats method

--- javascript/fundamentals/practice/ninja.js
+++ javascript/fundamentals/practice/ninja.js
@@ -9,8 +9,12 @@ class Ninja {
 	sayName() {
 		console.log(`This ninjas name is: ${this.name}`);
 	}
+
+    showStats() {
+        console.log(`${this.name} has a strength of ${this.strength}, speed of ${this.speed}, and health of ${this.health}.`);
+    }
 }
 
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
-// ninja1.showStats();
+ninja1.showStats();
