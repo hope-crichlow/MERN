e9c7882c5aa9fa9c901835117a99732af5aa1ab2
hope-crichlow <hopecrichlow@gmail.com>
2021-09-05 18:37:01 -0700

    drinkSake method

--- javascript/fundamentals/practice/ninja.js
+++ javascript/fundamentals/practice/ninja.js
@@ -10,11 +10,19 @@ class Ninja {
 		console.log(`This ninjas name is: ${this.name}`);
 	}
 
-    showStats() {
-        console.log(`${this.name} has a strength of ${this.strength}, speed of ${this.speed}, and health of ${this.health}.`);
-    }
+	showStats() {
+		console.log(
+			`${this.name} has a strength of ${this.strength}, speed of ${this.speed}, and health of ${this.health}.`
+		);
+	}
+
+	drinkSake() {
+		this.health += 10;
+	}
 }
 
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
 ninja1.showStats();
