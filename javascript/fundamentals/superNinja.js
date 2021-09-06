0c80370000d46000c3e44b2383ba761a24f33c67
hope-crichlow <hopecrichlow@gmail.com>
2021-09-05 18:54:21 -0700

    class Sensei inherits from the Ninja class

--- javascript/fundamentals/superNinja.js
+++ javascript/fundamentals/superNinja.js
@@ -25,3 +25,19 @@ ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+	constructor(name) {
+		super(name);
+		this.health = 200;
+		this.speed = 10;
+		this.strength = 10;
+		this.wisdom = 10;
+	}
+}
+// example output
+const superSensei = new Sensei("Master Splinter");
+// superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"copy
