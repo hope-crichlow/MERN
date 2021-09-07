f97231b7d856083e667746f052c1e3bdff71608f
hope-crichlow <hopecrichlow@gmail.com>
2021-09-07 10:18:36 -0700

    Person card rendering

---
+++ react/classComponents/propitup/src/components/Person.jsx
@@ -0,0 +1,19 @@
+// IMPORT REACT FROM OUR DEPENDENCIES
+import React, {Component} from "react";
+
+// CREATE THE COMPONENT
+class Person extends Component {
+
+    render(){
+        const {firstName, lastName, age, hairColor} = this.props;
+        return(
+            <div>
+                <h3>{lastName}, {firstName}</h3>
+                <p>Age: {age} </p>
+                <p>Hair Color: {hairColor}</p>
+            </div>
+        )
+    }
+}
+// EXPORT THE COMPONENT
+export default Person;
