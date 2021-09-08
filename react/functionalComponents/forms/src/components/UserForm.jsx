2c3fb108e171b0021c76165baf352bfe53e65a44
hope-crichlow <hopecrichlow@gmail.com>
2021-09-07 18:32:19 -0700

    Form input rendering in data section

--- react/functionalComponents/forms/src/components/UserForm.jsx
+++ react/functionalComponents/forms/src/components/UserForm.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 const UserForm = (props) => {
-	const [username, setUsername] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
 	const createUser = (e) => {
+		// STOP THE FORM FROM MAKING A REQUEST
 		e.preventDefault();
-		const newUser = { username, email, password };
+		const newUser = { firstName, lastName, email, password };
 		console.log("Welcome", newUser);
 	};
 
@@ -16,11 +18,20 @@ const UserForm = (props) => {
 			<h3>New User Form</h3>
 			<form className="mb3" onSubmit={createUser}>
 				<div>
-					<label className="form-label">Username: </label>
+					<label className="form-label">First Name: </label>
 					<input
 						className="form-control"
 						type="text"
-						onChange={(e) => setUsername(e.target.value)}
+						onChange={(e) => setFirstName(e.target.value)}
+						value={firstName}
+					/>
+				</div>
+				<div>
+					<label className="form-label">Last Name: </label>
+					<input
+						className="form-control"
+						type="text"
+						onChange={(e) => setLastName(e.target.value)}
 					/>
 				</div>
 				<div>
@@ -41,9 +52,41 @@ const UserForm = (props) => {
 				</div>
 				<input className="btn" type="submit" value="Create User" />
 			</form>
+
+			<h3>Your Form Data</h3>
+			<form action="">
+				<div class="row mb-3">
+					<label for="inputFirstName" class="col-sm-2 col-form-label">
+						First Name
+					</label>
+					<div class="col-sm-10">
+						<input
+							type="text"
+							class="form-control"
+							id="inputfirstName"
+							value={firstName}
+						/>
+					</div>
+				</div>
+				<div class="row mb-3">
+					<label for="inputEmail3" class="col-sm-2 col-form-label">
+						Email
+					</label>
+					<div class="col-sm-10">
+						<input type="email" class="form-control" id="inputEmail3" />
+					</div>
+				</div>
+				<div class="row mb-3">
+					<label for="inputPassword3" class="col-sm-2 col-form-label">
+						Password
+					</label>
+					<div class="col-sm-10">
+						<input type="password" class="form-control" id="inputPassword3" />
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
 
 export default UserForm;
-
