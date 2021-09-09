53930f739f47c80b95749f0f7509f1132b914611
hope-crichlow <hopecrichlow@gmail.com>
2021-09-09 14:14:39 -0700

    pokemon displaying

--- react/apis/pokemon/src/components/Main.jsx
+++ react/apis/pokemon/src/components/Main.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Main = () => {
-    const axiosCall = () => {
-        axios.get("https://pokeapi.co/api/v2/pokemon")
-        .then(response => console.log(response))
-        .catch(error => console.log(error))
-    }
-    return (
-        <div>
-            <button onClick={axiosCall}>Fetch Pokemon</button>
-        </div>
-    )
-}
+	const [state, setState] = useState();
 
-export default Main
+	const axiosCall = () => {
+		// MAKE GET REQUEST TO POKEMON API
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon/?limit=900")
+			// HANDLE SUCCESSFUL RESPONSE
+			.then((response) => setState(response.data.results))
+			// HANDLE UNSUCCESSFUL RESPONSE
+			.catch((error) => console.log(error));
+	};
+	return (
+		<div>
+			<button onClick={axiosCall}>Fetch Pokemon</button>
+			{state ? (
+				<ul>
+					{state.map((pokemon, i) => (
+						<li>{pokemon.name}</li>
+					))}
+				</ul>
+			) : (
+				<h1>Wanna catch some Pokemon?</h1>
+			)}
+		</div>
+	);
+};
+
+export default Main;
