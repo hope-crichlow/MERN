bf59e5d7d213239cde3422794b5287b1df712e15
hope-crichlow <hopecrichlow@gmail.com>
2021-09-12 19:19:18 -0700

    form onchange working

--- react/functionalComponents/todolist/src/components/Form.jsx
+++ react/functionalComponents/todolist/src/components/Form.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Form = () => {
-    return (
-			<div>
-				<fieldset>
-					<legend>Form.jsx</legend>
-				</fieldset>
-			</div>
-		);
-}
+	const [inputState, setInputState] = useState("");
+	return (
+		<div className="container px-4">
+			<fieldset>
+				<legend>Form.jsx</legend>
+				<form
+					className="form-control d-flex gap-5 justify-content-center "
+					action=""
+				>
+					{/* <label className="form-label"></label> */}
+					<input
+						className="form-control"
+						type="text"
+						onChange={(e)=> setInputState(e.target.value)}
+						name="task"
+						value={inputState}
+					/>
+					<button className="btn btn-primary" type="submit">
+						Add
+					</button>
+				</form>
+			</fieldset>
+		</div>
+	);
+};
 
 export default Form;
