4ab5e935d530cdd1bfa3f27ac3d13a04ec40f3ea
hope-crichlow <hopecrichlow@gmail.com>
2021-09-12 19:38:47 -0700

    Inputs being saved to array of tasks

--- react/functionalComponents/todolist/src/App.js
+++ react/functionalComponents/todolist/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Form from './components/Form';
 import ListDisplay from './components/ListDisplay';
+import {useState} from "react";
 
 function App() {
+  
+  const [arrState, setArrState] = useState([]);
+
+  // A FUNCTION THAT ADDS INPUT STRING TO CURRENT ARRAY OF TASKS
+  const addTaskToArray = (task) => {
+    setArrState([...arrState, task]);
+  }
+
   return (
 		<fieldset>
       <legend>App.js</legend>
 			<div className="App">
-				<Form />
+				<Form addTaskToArray={addTaskToArray}/>
 				<ListDisplay />
 			</div>
 		</fieldset>
--- react/functionalComponents/todolist/src/components/Form.jsx
+++ react/functionalComponents/todolist/src/components/Form.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
 	const [inputState, setInputState] = useState("");
+	const { addTaskToArray } = props;
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		addTaskToArray(inputState);
+		setInputState("");
+	};
 	return (
 		<div className="container px-4">
 			<fieldset>
 				<legend>Form.jsx</legend>
 				<form
 					className="form-control d-flex gap-5 justify-content-center "
-					action=""
+					onSubmit={submitHandler}
 				>
 					{/* <label className="form-label"></label> */}
 					<input
 						className="form-control"
 						type="text"
-						onChange={(e)=> setInputState(e.target.value)}
+						onChange={(e) => setInputState(e.target.value)}
 						name="task"
 						value={inputState}
 					/>
