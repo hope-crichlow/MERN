dda0c1f16f235eb9c09e7749538c4db2b55f8c06
hope-crichlow <hopecrichlow@gmail.com>
2021-09-14 20:10:16 -0700

    Server running

--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -1,8 +1,14 @@
-// 1. IMPORT EXPRESS AND CREATE INSTANCE OF EXPRESS SERVER 
+// 1. IMPORT EXPRESS, CREATE INSTANCE OF EXPRESS SERVER, AND DEFINE PORT
+const express = require("express");
+const app = express();
+const port = 8000;
 
 // 2. CONFIGURATION FOR YOUR EXPRESS SERVER
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 3. DEFINE ROUTES FOR YOUR EXPRESS SERVER
 
-// 4. RUN YOUR EXPRESS SERVER
 
+// 4. RUN YOUR EXPRESS SERVER
+app.listen(port, () => console.log("EPRESS SERVER RUNNING ON PORT: " + port));
