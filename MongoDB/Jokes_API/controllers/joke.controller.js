ce0eb1a9666fa4d37bd021bc672dd75459d42f69
hope-crichlow <hopecrichlow@gmail.com>
2021-09-17 15:31:09 -0700

    Export a function to create a joke

--- MongoDB/Jokes_API/controllers/joke.controller.js
+++ MongoDB/Jokes_API/controllers/joke.controller.js
@@ -1,5 +1,25 @@
+// IMPORT JOKE MODEL FROM MODEL FILE
 const Joke = require("../models/joke.model");
 
+// Export a function to test your API
 module.exports.test = (req, res) => {
 	res.json("TESTING");
 };
+
+//////////////////////// CREATE /////////////////////////
+// Export a function to create a joke
+module.exports.createJoke = (req, res) => {
+	Joke.create(req.body)
+		.then(newJoke => res.json(newJoke))	// SUCCESS
+		.catch(err => res.json(err))	// NOT SUCCESSFUL
+}
+//////////////////////// READ ///////////////////////////
+// Export a function to get all jokes
+// Export a function to get a single joke
+
+///////////////////////// UPDATE ////////////////////////
+// Export a function to update a joke
+
+
+//////////////////////// DELETE ////////////////////////
+// Export a function to delete a joke
--- MongoDB/Jokes_API/models/joke.model.js
+++ MongoDB/Jokes_API/models/joke.model.js
@@ -12,13 +12,13 @@ const JokeSchema = new mongoose.Schema({  // Structre of a joke object
   // PUNCHLINE: STRING, REQUIRED, MIN: 3 CHARACTERS
   punchline: {
     type: String,
-    required: [true, "Can't have a joke without a setup"],
-    minlength: [10, "Setup must be at least 10 characters long"]
+    required: [true, "Is it really a joke if there isn't a punchline?"],
+    minlength: [3, "Punchline must be at least 3 characters long"]
   }
 },{ timestamps: true });  // Creates createdAt and updatedAt attributes
 
 // 2. REGISTERING THE SCHEMA W/I MONGOOSE LIBRARY
 const Joke = mongoose.model("Joke", JokeSchema);  // Pass in collection name and schema
 
-// 3. EXPORT MODEL
+// 3. EXPORT MONGOOSE MODEL
 module.exports = Joke;
