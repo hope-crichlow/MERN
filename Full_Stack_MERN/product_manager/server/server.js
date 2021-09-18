a6d7855b49de28987d5168213d5597796b63d61d
hope-crichlow <hopecrichlow@gmail.com>
2021-09-17 22:13:01 -0700

    Epress boilerplate running

---
+++ Full_Stack_MERN/product_manager/server/server.js
@@ -0,0 +1,20 @@
+// 1. IMPORT DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+
+// 1.5 CONFIGURE MONGOOSE
+// require("./config/mongoose.config");
+
+// 2. CONFIGURE EXPRESS
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+// 3. ATTACH ROUTES TO EXPRESS SERVER
+// const productRoutes = require("./routes/product.routes");
+// productRoutes(app);
+
+// 4. RUN EXPRESS SERVER
+app.listen(port, () => console.log(`EXPRESS SERVER RUNING ON ${port}`));
